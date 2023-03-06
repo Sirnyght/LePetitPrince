d2106a1594f9ef004fafab387db59e1a18716b28
Kytsune <kyytsune@gmail.com>
2023-03-06 22:11:10 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.lepetitprince',
+  appName: 'lepetitprince',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
